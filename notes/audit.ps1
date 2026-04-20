3349be82d74d1403cd51f585e669ea2d2ccd885e
Ganpati kumar <aryanf192811@gmail.com>
2026-04-20 19:40:44 +0530

    chore: remove temp audit script

--- notes/audit.ps1
+++
@@ -1,5 +0,0 @@
-$html = Get-Content 'neet-ecology-complete.html' -Raw
-$opens = ([regex]::Matches($html, '<div')).Count
-$closes = ([regex]::Matches($html, '</div>')).Count
-$diff = $opens - $closes
-Write-Host "Open divs: $opens, Close divs: $closes, Diff: $diff"
